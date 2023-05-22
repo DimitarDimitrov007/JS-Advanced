cea0e68b82fce84738e9893266182752a83de9ac
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-05-22 10:40:39 +0300

    Add files via upload

---
+++ Advanced-Arrays-and-Nested-Arrays-Lab/Process Odd Positions.js
@@ -0,0 +1,16 @@
+function processOddPositions(array){
+    let result = [];
+
+    for(let i = 0; i < array.length; i++){
+        if( i % 2 !== 0 ){
+            result.push(Number(array[i] * 2));
+
+        }
+    }
+
+    console.log(result.reverse().join(" "));
+
+
+}
+processOddPositions([10, 15, 20, 25]);
+processOddPositions([3, 0, 10, 4, 7, 3]);
