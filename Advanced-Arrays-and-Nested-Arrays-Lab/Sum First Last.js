78029b8b2c40a82b250615f300579e300ef93c2c
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-05-22 10:41:07 +0300

    Add files via upload

---
+++ Advanced-Arrays-and-Nested-Arrays-Lab/Sum First Last.js
@@ -0,0 +1,8 @@
+function sumFirstLast(array){
+   let first = Number(array[0]);
+   let last = Number(array[array.length - 1]);
+   console.log(first + last);
+
+}
+sumFirstLast(['20', '30', '40']);
+sumFirstLast(['5', '10']);
