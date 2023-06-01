51509351cd05fc7cf930753d948a5057cc093b79
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-06-01 21:29:00 +0300

    Add files via upload

---
+++ DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+   let hiddenElement = document.getElementById("more");
+   hiddenElement.style.display = "none";
+   let text = document.getElementById("text");
+   text.style.display = "inline";
+}
